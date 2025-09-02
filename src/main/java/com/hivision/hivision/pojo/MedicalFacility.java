b4252892a5307354e638c81db6412e9992cf4b53
MinhHieu <minhieuu1@gmail.com>
2025-09-02 09:22:49 +0700

    update feat: update medical facility

--- src/main/java/com/hivision/hivision/pojo/MedicalFacility.java
+++ src/main/java/com/hivision/hivision/pojo/MedicalFacility.java
@@ -3,6 +3,7 @@ package com.hivision.hivision.pojo;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
@@ -32,4 +33,23 @@ public class MedicalFacility {
     @Column(name = "Phone", length = 50)
     String phone;
 
+    @Size(max = 255)
+    @Column(name = "Img")
+    String img;
+
+    @Size(max = 50)
+    @Column(name = "\"Time\"", length = 50)
+    String time;
+
+    @Size(max = 10)
+    @Column(name = "Rating", length = 10)
+    String rating;
+
+    @Lob
+    @Column(name = "Latitude")
+    String latitude;
+
+    @Lob
+    @Column(name = "Longitude")
+    String longitude;
 }
